d5060d4ad60f4a1e1fb0c034b65850321c7315bd
Toru Tamura <torut@ttsoft.jp>
2013-06-14 16:31:46 +0900

    remove linetail whitespaces.

--- githubwordpress.php
+++ githubwordpress.php
@@ -11,25 +11,25 @@ License: GPL2
 
 class githubwordpress extends WP_Widget {
 	function githubwordpress() {
-		
+
 		require(dirname(__FILE__) . "/languages/" . get_bloginfo('language') . "/index.php");
-	
+
 		$options = array( 'classname' => 'githubwordpress', 'description' => __( $github_description ) );
 		$this->WP_Widget('githubwordpress', __($github_name), $options);
 	}
-	
+
 	function form($instance) {
-		
+
 		if(file_exists(dirname(__FILE__) . "/languages/" . get_bloginfo('language') . "/index.php")){
-		
+
 			require(dirname(__FILE__) . "/languages/" . get_bloginfo('language') . "/index.php");
-			
+
 		}else{
-		
+
 			require(dirname(__FILE__) . "/languages/en-US/index.php");
-		
+
 		}
-		
+
 		echo '<div id="githubwordpress-widget-form">';
 		echo '<p><label for="' . $this->get_field_id("username") .'">' . $github_username . ' :</label>';
 		echo '<input type="text" name="' . $this->get_field_name("username") . '" ';
@@ -54,9 +54,9 @@ class githubwordpress extends WP_Widget {
 	}
 
 	function widget($args, $instance) {
-	
+
 		require(dirname(__FILE__) . "/languages/" . get_bloginfo('language') . "/index.php");
-	
+
 		if ( isset($instance['error']) && $instance['error'] )
 			return;
 
@@ -64,55 +64,55 @@ class githubwordpress extends WP_Widget {
 			$before_title = $args['before_title'];
 		else
 			$before_title = '<h3 class="widget-title">';
-		
+
 		if(isset($args['after_title']))
 			$after_title = $args['after_title'];
 		else
 			$after_title = '</h3>';
-		
+
 		if(isset($args['before_widget']))
 			$before_widget = $args['before_widget'];
 		else
 			$before_widget = '';
-		
+
 		if(isset($args['after_widget']))
 			$after_widget = $args['after_widget'];
 		else
 			$after_widget = '';
-		
+
 		$user = $instance['username'];
 		$password = $instance['password'];
-		
+
 		if(!empty($password)){
-		
+
 			$headers = array(
 				"Authorization: Basic " . base64_encode( $user . ":" . $password)
 			);
-			
+
 		}
-		
+
 		$url = "https://api.github.com/users/" . $user . "/repos";
-		
+
 		// set URL and other appropriate options
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		
+
 		if(!empty($password)){
-		
+
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			
+
 		}
-		
+
 		// grab URL and pass it to the browser
 		$data = curl_exec($ch);
-		
+
 		curl_setopt($ch, CURLOPT_URL, $url);
 		$data = curl_exec($ch);
 		$json = json_decode($data);
-		
+
 		echo $before_widget;
 		echo $before_title;
 
@@ -140,7 +140,7 @@ class githubwordpress extends WP_Widget {
 				echo $github_show_string . '</a></p>';
 				echo '<div id="githublistdiv" style="display:none"><ul id="githublist">';
 			}
-			
+
 			foreach($json as $repo) {
 				if (isset($json->message)) {
                                         echo $github_error . " " . $json->message;
@@ -158,44 +158,42 @@ class githubwordpress extends WP_Widget {
 
 				foreach($repo as $coder) {
 					$total++;
-					
+
 					if($coder->committer->login == $user)
 						$counter++;
 				}
 
 				if($counter==0){
-				
+
 					echo  "0 " . $github_percent_string . "</li>";
-				
+
 				}else{
 
 					echo (int) (($counter / $total) * 100) . " " . $github_percent_string . "</li>";
-					
+
 				}
 				unset($coder);
 			}
-		
+
 		curl_close($ch);
 		echo "</ul></div>";
 		echo $after_widget;
 	}
-	
+
 	function update($new_instance, $old_instance) {
-		$instance = $old_instance;		
+		$instance = $old_instance;
 		$instance['username'] = strip_tags($new_instance['username']);
 		$instance['password'] = strip_tags($new_instance['password']);
 		$instance['hidden'] = strip_tags($new_instance['hidden']);
 		return $instance;
-	}		
-	
+	}
+
 }
 
 add_action('widgets_init', create_function('', 'return register_widget("githubwordpress");'));
 add_action("wp_head","github_add_scripts");
-	
+
 function github_add_scripts() {
 	echo '<link rel="stylesheet" href="' . plugins_url("/css/github_wordpress_widget.css", __FILE__ ) . '" />';
-    echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>';
-;
+	echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>';
 }
-?>
